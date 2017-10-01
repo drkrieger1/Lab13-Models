62d3af5fd3a3c0b5b1420214076dc1805a408952
drkrieger <eplyushko@gmail.com>
2017-10-01 15:09:37 -0700

    register scafold done with taghelpers

---
+++ Lab13Erik/Lab13Erik/Data/Lab13ErikContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lab13Erik.Model;
+
+namespace Lab13Erik.Models
+{
+    public class Lab13ErikContext : DbContext
+    {
+        public Lab13ErikContext (DbContextOptions<Lab13ErikContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab13Erik.Model.Student> Student { get; set; }
+    }
+}
--- Lab13Erik/Lab13Erik/Startup.cs
+++ Lab13Erik/Lab13Erik/Startup.cs
@@ -6,16 +6,28 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Lab13Erik.Models;
 
 namespace Lab13Erik
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<Lab13ErikContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("Lab13ErikContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
